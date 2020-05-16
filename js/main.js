ef6d6bc3801ab940bcddd55e8768d423d6f6ccd4
Karina Shvets <shvets.karina13@gmail.com>
2020-05-16 22:18:54 +0300

    Add accordeon and styles

--- js/main.js
+++ js/main.js
@@ -1,8 +1,54 @@
 $(function() {
+    
 // Выпадающее меню
 $('.menu-btn').on('click', function(e) {
-  e.preventDefault();
-  $('.menu').toggleClass('menu-active');
+    e.preventDefault();
+    $('.menu').toggleClass('menu-active');
 })
+
+//slick-slider
+$('.reviews-slider').slick({
+    dots: false,
+    arrows: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    waitForAnimate: false,  
+});
+
+//Accordion 
+$(function() {
+  var Accordion = function(el, multiple) {
+    this.el = el || {};
+    // more then one submenu open?
+    this.multiple = multiple || false;
+    
+    var dropdownlink = this.el.find('.dropdownlink');
+    dropdownlink.on('click',
+                    { el: this.el, multiple: this.multiple },
+                    this.dropdown);
+    $('.t1').click();
+  };
+  
+  Accordion.prototype.dropdown = function(e) {
+    var $el = e.data.el,
+        $this = $(this),
+        //this is the ul.submenuItems
+        $next = $this.next();
+    
+    $next.slideToggle();
+    $this.parent().toggleClass('open');
+    
+    if(!e.data.multiple) {
+      //show only one menu at the same time
+      $el.find('.submenuItems').not($next).slideUp().parent().removeClass('open');
+    }
+  }
+  
+  var accordion = new Accordion($('.accordion-menu'), false);
+})
+
     
 });
