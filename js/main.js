7e062d2ed3ccf2baabfa54d293b6f8f28280db19
Karina Shvets <shvets.karina13@gmail.com>
2020-06-07 16:44:55 +0300

    Update style.css and index.html

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@ $('.reviews-slider').slick({
     infinite: true,
     slidesToShow: 1,
     slidesToScroll: 1,
-    autoplay: true,
+    autoplay: false,
     autoplaySpeed: 2000,
     waitForAnimate: false,  
     responsive: [
